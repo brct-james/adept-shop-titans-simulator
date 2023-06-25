44a8a962a479137d568976a2f648567d2a183a79
brct-james <james.clague@brct.io>
2023-06-25 18:17:37 +0000

    Bump version

--- build_release.sh
+++ build_release.sh
@@ -6,7 +6,7 @@ mkdir -p releases/build/adept/adept_data
 cp -r target/x86_64-pc-windows-gnu/release/adept_shop_titans_simulator.exe releases/build/adept/adept_shop_titans_simulator.exe
 cp -r bundle releases/build/adept/adept_data/bundle
 cp -r config releases/build/adept/adept_data/config
-mkdir -p releases/0.1.0
+mkdir -p releases/0.1.1
 cd releases/build
-zip -r ../../releases/0.1.0/adept_0.1.0_windows.zip adept
+zip -r ../../releases/0.1.1/adept_0.1.1_windows.zip adept
 cd ../..
