a08a11dbb4b47e5c7997996c2aa9dd8597fb2e0f
brct-james <james.clague@brct.io>
2023-06-25 17:34:26 +0000

    Change release structure

--- build_release.sh
+++ build_release.sh
@@ -1,8 +1,12 @@
 clear
 cargo fmt
 cross build --bin adept_shop_titans_simulator --release --target x86_64-pc-windows-gnu
-rm -rf release
-mkdir release
-cp -r target/x86_64-pc-windows-gnu/release/adept_shop_titans_simulator.exe release/adept_shop_titans_simulator.exe
-cp -r bundle release/bundle
-cp -r config release/config
+rm -rf releases
+mkdir -p releases/build/adept/adept_data
+cp -r target/x86_64-pc-windows-gnu/release/adept_shop_titans_simulator.exe releases/build/adept/adept_shop_titans_simulator.exe
+cp -r bundle releases/build/adept/adept_data/bundle
+cp -r config releases/build/adept/adept_data/config
+mkdir -p releases/0.1.0
+cd releases/build
+zip -r ../../releases/0.1.0/adept_0.1.0_windows.zip adept
+cd ../..
