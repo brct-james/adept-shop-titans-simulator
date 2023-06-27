b35ec05c63c2096792bbdd06b74ab8895eeaa337
brct-james <james.clague@brct.io>
2023-06-27 14:35:03 +0000

    Add version number to future releases of the exe

--- build_release.sh
+++ build_release.sh
@@ -6,7 +6,7 @@ cargo fmt
 cross build --bin adept_shop_titans_simulator --release --target x86_64-pc-windows-gnu
 rm -rf releases/build
 mkdir -p releases/build/adept/adept_data
-cp -r target/x86_64-pc-windows-gnu/release/adept_shop_titans_simulator.exe releases/build/adept/adept_shop_titans_simulator.exe
+cp -r target/x86_64-pc-windows-gnu/release/adept_shop_titans_simulator.exe releases/build/adept/adept_shop_titans_simulator_v${currentversion}.exe
 cp -r bundle releases/build/adept/adept_data/bundle
 cp -r config releases/build/adept/adept_data/config
 mkdir -p releases/v${currentversion}
