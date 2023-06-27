a466b489f99a8464f5ef2fbc0b664baf705d710b
brct-james <james.clague@brct.io>
2023-06-27 01:13:01 +0000

    Final change to build format

--- build_release.sh
+++ build_release.sh
@@ -9,8 +9,8 @@ mkdir -p releases/build/adept/adept_data
 cp -r target/x86_64-pc-windows-gnu/release/adept_shop_titans_simulator.exe releases/build/adept/adept_shop_titans_simulator.exe
 cp -r bundle releases/build/adept/adept_data/bundle
 cp -r config releases/build/adept/adept_data/config
-mkdir -p releases/${currentversion}
+mkdir -p releases/v${currentversion}
 cd releases/build
-zip -r ../../releases/${currentversion}/adept_${currentversion}_windows.zip adept
+zip -r ../../releases/v${currentversion}/adept_v${currentversion}_windows.zip adept
 cd ../..
 echo "Build Complete"
